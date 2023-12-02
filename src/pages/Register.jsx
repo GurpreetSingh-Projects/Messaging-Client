2d738c57d0bb499dbb57d92c75963dfd81b31d97
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-12-03 02:22:20 +0530

    fixed tailwind installation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,20 +1,18 @@
 export default function Register() {
   return (
-    <>
-      <div className="formContainer">
-        <div className="formWrapper">
-          <span className="logo">Messaging Client</span>
-          <span className="title">Register</span>
-          <form>
-            <input type="text" placeholder="Enter username"></input>
-            <input type="email" placeholder="Email"></input>
-            <input type="password" placeholder="Passoword"></input>
-            <input type="file"></input>
-            <button>Sign up</button>
-          </form>
-          <p>Already have an account? Login</p>
-        </div>
+    <div className="flex h-100vh place-content-center align-center justify-center bg-blue-400 m-auto">
+      <div className="flex">
+        <span className="hi">Messaging Client</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="Enter username"></input>
+          <input type="email" placeholder="Email"></input>
+          <input type="password" placeholder="Passoword"></input>
+          <input type="file"></input>
+          <button>Sign up</button>
+        </form>
+        <p>Already have an account? Login</p>
       </div>
-    </>
+    </div>
   );
 }
