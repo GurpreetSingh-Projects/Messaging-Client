89db03a2f323e18d8b18f060c112f73e961027aa
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-12-06 19:40:31 +0530

    code updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,7 +8,8 @@ export default function Register() {
           <input type="text" placeholder="Enter username"></input>
           <input type="email" placeholder="Email"></input>
           <input type="password" placeholder="Passoword"></input>
-          <input type="file"></input>
+          <input type="file" id="file" style={{ display: "none" }}></input>
+          <label htmlFor="file"></label>
           <button>Sign up</button>
         </form>
         <p>Already have an account? Login</p>
