0b11f7d337355dc52a8323c943e4ec706d090356
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-12-07 19:04:14 +0530

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./style.scss";
 import Register from "./pages/register";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 function App() {
   return (
     <>
       <div>
+        <Home />
+        {/* <Login /> */}
         {/* <Register /> */}
-        <Login />
       </div>
     </>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="home">
+        <div className="homeWrapper"></div>
+      </div>
+    </div>
+  );
+}
