de96501372fa5f2a4e6ed6926130670fc5b7d81c
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-12-07 20:26:11 +0530

    code update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,12 @@
 export default function Navbar() {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <span className="logo">Messaging Client</span>
+      <div className="user">
+        <img src="" alt="" />
+        <span>John</span>
+        <button>Logout</button>
+      </div>
+    </div>
+  );
 }
