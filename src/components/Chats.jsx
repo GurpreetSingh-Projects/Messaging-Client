277342de718d1fbe957d49c6d7a3ee0570590b7d
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-12-11 17:46:02 +0530

    added chats component

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,3 +1,16 @@
 export default function Chats() {
-  return <div>Chats</div>;
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/14198675/pexels-photo-14198675.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>hello</p>
+        </div>
+      </div>
+    </div>
+  );
 }
