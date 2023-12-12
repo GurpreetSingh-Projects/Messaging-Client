d1aeac79cef67e98a4af4dc586179bb6410170ee
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-12-13 01:33:43 +0530

    code update

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,3 +1,17 @@
+import Cam from "../img/cam.png";
+import Add from "../img/cam.png";
+import More from "../img/more.png";
 export default function Chat() {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Jane</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
