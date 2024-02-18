9de75dab265a25ce738c0a96f7b163c4aa90750c
Gurpreet Singh <gpsingh02414@gmail.com>
2024-02-18 20:58:29 +0530

    commit

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -11,6 +11,36 @@ export default function Chats() {
           <p>hello</p>
         </div>
       </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/14198675/pexels-photo-14198675.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/14198675/pexels-photo-14198675.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/14198675/pexels-photo-14198675.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>hello</p>
+        </div>
+      </div>
     </div>
   );
 }
