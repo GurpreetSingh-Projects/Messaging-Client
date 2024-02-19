0714a24b1ef3b00ec1c38c389dcf48710511b29c
Gurpreet Singh <gpsingh02414@gmail.com>
2024-02-19 23:55:02 +0530

    improved ui

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,3 +1,20 @@
 export default function Message() {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/19068827/pexels-photo-19068827/free-photo-of-woman-holding-elegant-bouquet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img
+          src="https://images.pexels.com/photos/19068827/pexels-photo-19068827/free-photo-of-woman-holding-elegant-bouquet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
