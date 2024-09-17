8f4691e9fa7aedf73543a30ebbbd43b09b1ee63d
Gurpreet Singh <gpsingh02414@gmail.com>
2024-09-18 02:16:10 +0530

    changes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./style.scss";
-import Register from "./pages/register";
+import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
