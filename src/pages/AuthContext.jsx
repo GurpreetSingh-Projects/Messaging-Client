865f037f0b08f46202ce4a821611618bdc0e18b1
Gurpreet Singh <gpsingh02414@gmail.com>
2024-11-29 23:00:00 +0530

    code updated

--- src/pages/AuthContext.jsx
+++ src/pages/AuthContext.jsx
@@ -1,19 +1,19 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
-import { auth } from "../firebase ";
+// import { auth } from "../firebase ";
 export const AuthContext = createContext();
 export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState({});
 
-  useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user);
-      console.log(user);
-    });
-    return () => {
-      unsub();
-    };
-  }, []);
+  // useEffect(() => {
+  //   const unsub = onAuthStateChanged(auth, (user) => {
+  //     setCurrentUser(user);
+  //     console.log(user);
+  //   });
+  //   return () => {
+  //     unsub();
+  //   };
+  // }, []);
   return (
     <AuthContext.Provider value={{ currentUser }}>
       {children}
